2977ee90f8a5724ce1b4d6db80159dab50496fd5
conversaShawn <shawn.harris3510@gmail.com>
2022-02-17 15:01:21 -0800

    should stop after 5 failed tests

--- cypress/integration/app1f.spec.js
+++ cypress/integration/app1f.spec.js
@@ -18,157 +18,7 @@ describe("example to-do app", () => {
     cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
 
     cy.get(".todo-list li")
-      .should("have.length", 3)
-      .last()
-      .should("have.text", newItem);
-  });
-
-  it("can check off an item as completed", () => {
-    cy.contains("Pay electric bill")
-      .parent()
-      .find("input[type=checkbox]")
-      .check();
-
-    cy.contains("Pay electric bill")
-      .parents("li")
-      .should("have.class", "completed");
-  });
-
-  describe("with a checked task", () => {
-    beforeEach(() => {
-      cy.contains("Pay electric bill")
-        .parent()
-        .find("input[type=checkbox]")
-        .check();
-    });
-
-    it("can filter for uncompleted tasks", () => {
-      cy.contains("Active").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Walk the dog");
-
-      cy.contains("Pay electric bill").should("not.exist");
-    });
-
-    it("can filter for completed tasks", () => {
-      cy.contains("Completed").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Pay electric bill");
-
-      cy.contains("Walk the dog").should("not.exist");
-    });
-
-    it("can delete all completed tasks", () => {
-      cy.contains("Clear completed").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .should("not.have.text", "Pay electric bill");
-
-      cy.contains("Clear completed").should("not.exist");
-    });
-  });
-});
-describe("example to-do app", () => {
-  beforeEach(() => {
-    cy.visit("https://example.cypress.io/todo");
-  });
-
-  it("displays two todo items by default", () => {
-    cy.get(".todo-list li").should("have.length", 2);
-
-    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
-    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
-  });
-
-  it("can add new todo items", () => {
-    const newItem = "Feed the cat";
-
-    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
-
-    cy.get(".todo-list li")
-      .should("have.length", 3)
-      .last()
-      .should("have.text", newItem);
-  });
-
-  it("can check off an item as completed", () => {
-    cy.contains("Pay electric bill")
-      .parent()
-      .find("input[type=checkbox]")
-      .check();
-
-    cy.contains("Pay electric bill")
-      .parents("li")
-      .should("have.class", "completed");
-  });
-
-  describe("with a checked task", () => {
-    beforeEach(() => {
-      cy.contains("Pay electric bill")
-        .parent()
-        .find("input[type=checkbox]")
-        .check();
-    });
-
-    it("can filter for uncompleted tasks", () => {
-      cy.contains("Active").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Walk the dog");
-
-      cy.contains("Pay electric bill").should("not.exist");
-    });
-
-    it("can filter for completed tasks", () => {
-      cy.contains("Completed").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Pay electric bill");
-
-      cy.contains("Walk the dog").should("not.exist");
-    });
-
-    it("can delete all completed tasks", () => {
-      cy.contains("Clear completed").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .should("not.have.text", "Pay electric bill");
-
-      cy.contains("Clear completed").should("not.exist");
-    });
-  });
-});
-describe("example to-do app", () => {
-  beforeEach(() => {
-    cy.visit("https://example.cypress.io/todo");
-  });
-
-  it("displays two todo items by default", () => {
-    cy.get(".todo-list li").should("have.length", 2);
-
-    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
-    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
-  });
-
-  it("can add new todo items", () => {
-    const newItem = "Feed the cat";
-
-    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
-
-    cy.get(".todo-list li")
-      .should("have.length", 3)
+      .should("have.length", 30)
       .last()
       .should("have.text", newItem);
   });
@@ -306,7 +156,7 @@ describe("example to-do app", () => {
   });
 
   it("displays two todo items by default", () => {
-    cy.get(".todo-list li").should("have.length", 20);
+    cy.get(".todo-list li").should("have.length", 2);
 
     cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
     cy.get(".todo-list li").last().should("have.text", "Walk the dog");
@@ -346,7 +196,7 @@ describe("example to-do app", () => {
       cy.contains("Active").click();
 
       cy.get(".todo-list li")
-        .should("have.length", 10)
+        .should("have.length", 1)
         .first()
         .should("have.text", "Walk the dog");
 
@@ -357,7 +207,7 @@ describe("example to-do app", () => {
       cy.contains("Completed").click();
 
       cy.get(".todo-list li")
-        .should("have.length", 10)
+        .should("have.length", 1)
         .first()
         .should("have.text", "Pay electric bill");
 
@@ -368,7 +218,7 @@ describe("example to-do app", () => {
       cy.contains("Clear completed").click();
 
       cy.get(".todo-list li")
-        .should("have.length", 10)
+        .should("have.length", 1)
         .should("not.have.text", "Pay electric bill");
 
       cy.contains("Clear completed").should("not.exist");
@@ -381,7 +231,7 @@ describe("example to-do app", () => {
   });
 
   it("displays two todo items by default", () => {
-    cy.get(".todo-list li").should("have.length", 20);
+    cy.get(".todo-list li").should("have.length", 2);
 
     cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
     cy.get(".todo-list li").last().should("have.text", "Walk the dog");
@@ -421,7 +271,7 @@ describe("example to-do app", () => {
       cy.contains("Active").click();
 
       cy.get(".todo-list li")
-        .should("have.length", 10)
+        .should("have.length", 1)
         .first()
         .should("have.text", "Walk the dog");
 
@@ -432,7 +282,7 @@ describe("example to-do app", () => {
       cy.contains("Completed").click();
 
       cy.get(".todo-list li")
-        .should("have.length", 10)
+        .should("have.length", 1)
         .first()
         .should("have.text", "Pay electric bill");
 
@@ -443,7 +293,7 @@ describe("example to-do app", () => {
       cy.contains("Clear completed").click();
 
       cy.get(".todo-list li")
-        .should("have.length", 10)
+        .should("have.length", 1)
         .should("not.have.text", "Pay electric bill");
 
       cy.contains("Clear completed").should("not.exist");
@@ -468,7 +318,7 @@ describe("example to-do app", () => {
     cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
 
     cy.get(".todo-list li")
-      .should("have.length", 3)
+      .should("have.length", 30)
       .last()
       .should("have.text", newItem);
   });
@@ -543,7 +393,7 @@ describe("example to-do app", () => {
     cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
 
     cy.get(".todo-list li")
-      .should("have.length", 3)
+      .should("have.length", 30)
       .last()
       .should("have.text", newItem);
   });
@@ -618,7 +468,7 @@ describe("example to-do app", () => {
     cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
 
     cy.get(".todo-list li")
-      .should("have.length", 3)
+      .should("have.length", 30)
       .last()
       .should("have.text", newItem);
   });
@@ -693,7 +543,157 @@ describe("example to-do app", () => {
     cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
 
     cy.get(".todo-list li")
-      .should("have.length", 3)
+      .should("have.length", 30)
+      .last()
+      .should("have.text", newItem);
+  });
+
+  it("can check off an item as completed", () => {
+    cy.contains("Pay electric bill")
+      .parent()
+      .find("input[type=checkbox]")
+      .check();
+
+    cy.contains("Pay electric bill")
+      .parents("li")
+      .should("have.class", "completed");
+  });
+
+  describe("with a checked task", () => {
+    beforeEach(() => {
+      cy.contains("Pay electric bill")
+        .parent()
+        .find("input[type=checkbox]")
+        .check();
+    });
+
+    it("can filter for uncompleted tasks", () => {
+      cy.contains("Active").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", "Walk the dog");
+
+      cy.contains("Pay electric bill").should("not.exist");
+    });
+
+    it("can filter for completed tasks", () => {
+      cy.contains("Completed").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", "Pay electric bill");
+
+      cy.contains("Walk the dog").should("not.exist");
+    });
+
+    it("can delete all completed tasks", () => {
+      cy.contains("Clear completed").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .should("not.have.text", "Pay electric bill");
+
+      cy.contains("Clear completed").should("not.exist");
+    });
+  });
+});
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/todo");
+  });
+
+  it("displays two todo items by default", () => {
+    cy.get(".todo-list li").should("have.length", 2);
+
+    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
+    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
+  });
+
+  it("can add new todo items", () => {
+    const newItem = "Feed the cat";
+
+    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
+
+    cy.get(".todo-list li")
+      .should("have.length", 30)
+      .last()
+      .should("have.text", newItem);
+  });
+
+  it("can check off an item as completed", () => {
+    cy.contains("Pay electric bill")
+      .parent()
+      .find("input[type=checkbox]")
+      .check();
+
+    cy.contains("Pay electric bill")
+      .parents("li")
+      .should("have.class", "completed");
+  });
+
+  describe("with a checked task", () => {
+    beforeEach(() => {
+      cy.contains("Pay electric bill")
+        .parent()
+        .find("input[type=checkbox]")
+        .check();
+    });
+
+    it("can filter for uncompleted tasks", () => {
+      cy.contains("Active").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", "Walk the dog");
+
+      cy.contains("Pay electric bill").should("not.exist");
+    });
+
+    it("can filter for completed tasks", () => {
+      cy.contains("Completed").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", "Pay electric bill");
+
+      cy.contains("Walk the dog").should("not.exist");
+    });
+
+    it("can delete all completed tasks", () => {
+      cy.contains("Clear completed").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .should("not.have.text", "Pay electric bill");
+
+      cy.contains("Clear completed").should("not.exist");
+    });
+  });
+});
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/todo");
+  });
+
+  it("displays two todo items by default", () => {
+    cy.get(".todo-list li").should("have.length", 2);
+
+    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
+    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
+  });
+
+  it("can add new todo items", () => {
+    const newItem = "Feed the cat";
+
+    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
+
+    cy.get(".todo-list li")
+      .should("have.length", 30)
       .last()
       .should("have.text", newItem);
   });
